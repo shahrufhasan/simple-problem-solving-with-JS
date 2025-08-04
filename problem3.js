046e0ae27ca896f43ff6795ab0e2158c66049f09
Shahruf hasan <info.shahrufhasan@gmail.com>
2025-08-04 13:58:36 +0600

    v3

--- problem3.js
+++ problem3.js
@@ -1,5 +1,5 @@
 /** Problem -03 ( Medicine Planner ) */
-var lastDay = 30;
+var lastDay = 11;
 //write your code here
 for (var i = 1; i <= lastDay; i++) {
   if (i % 3 === 0) {
--- problem5.js
+++ problem5.js
@@ -3,6 +3,6 @@ var student = { name: "jhankar", roll: 1014, department: "cse" };
 //write your code here
 
 var info = Object.values(student);
-var email = info.join("");
-var final = email + "@ph.ac.bd";
-console.log(final);
+var combine = info.slice(0, 2).join("");
+var email = [combine, info[2]].join(".") + "@ph.ac.bd";
+console.log(email);
